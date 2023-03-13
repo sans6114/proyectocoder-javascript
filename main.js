71b80c13f74294fb9d0f92da6090868116c1a123
Santiago Sosa <santisosalp@gmail.com>
2023-03-13 16:24:19 -0300

    se agrego algoritmo de envío

--- main.js
+++ main.js
@@ -4,10 +4,10 @@
 
 //Mensaje de bienvenida para el usuario//
 
-alert("Hola! bienvenido al carrito de compras de ediciones VS.\nIngrese a continuación el número del producto en el que estes interesado, si deseas salir ingresa 0 en tu teclado.")
+alert("Hola! Bienvenido al carrito de compras de ediciones VS.\nIngresa a continuación el número del producto en el que estés interesado, si deseas salir ingresa 0 en tu teclado.")
 
 //selección de productos de parte del usuario junto al mensaje//
-let seleccionarProductos = Number(prompt("1-¿Qué tienes en tu mano? (Mariana Maillet) valor: $1500\n2-Antidotos contra la religión (Juan Pablo Sosa) valor: $1500\n3-Mujer Maravilla (Natalia Spetale) valor: $1500\n4-Hijos de la Promesa (Ezequiel Rossini) valor: $1500\n5-La vida de Grace (Natalia Spetale) valor: $1500\n6-Diseñados para una vida Saludable (María José Amiunes) valor: $1500"))
+let seleccionarProductos = Number(prompt("1-¿Qué tienes en tu mano? (Mariana Maillet) valor: $1500\n2-Antídotos contra la religión (Juan Pablo Sosa) valor: $1500\n3-Mujer Maravilla (Natalia Spetale) valor: $1500\n4-Hijos de la Promesa (Ezequiel Rossini) valor: $1500\n5-La vida de Grace (Natalia Spetale) valor: $1500\n6-Diseñados para una vida Saludable (María José Amiunes) valor: $1500"))
 
 
 let seleccionarCantidad;
@@ -26,11 +26,7 @@ const cantidad = (cant, precio) => {
     return cant * precio
 }
 
-//función para envio del producto//
-function envio(){
-    let envio = prompt("Por último, ¿cómo quieres recibir el producto?")
 
-}
 
 while (seleccionarProductos != 0) {
     switch (seleccionarProductos) {
@@ -80,3 +76,34 @@ while (seleccionarProductos != 0) {
 }
 
 alert(`¡Gracias por tu compra! Has adquirido productos en Ediciones VS.\nPrecio de cada libro: $1500\nEl total de tu compra es: $${total}. ¡Esperamos que disfrutes de tu compra!`);
+
+//algoritmo para envío//
+
+const realizarEnvio = () => {
+    if (total >= 3000) {
+
+        alert("Buenas noticias, por haber comprado mas de 1 producto tienes el envío gratis, a continuación ingrese los datos pedidos para poder hacer el envío posible.");
+
+        const nombre = prompt("Nombre completo:")
+        const direcciónCompleta = prompt("Dirección completa, incluyendo calle, número de casa o apartamento, ciudad, estado o provincia:");
+        const codigoPostal = prompt("Codigo Postal:");
+        const teléfono = prompt("Número telefónico:");
+
+        alert(`Los datos ingresados fueron:\nNombre: ${nombre}\nDirección: ${direcciónCompleta}\nCodigo postal: ${codigoPostal}\nNúmero de teléfono: ${teléfono}\nEn unos instantes te llamaremos para concretar el envío, gracias por tu compra.`);
+    }
+    else if (total >= 1500) {
+
+        alert(`¿Sabes que si compras uno o más articulos ademas del que ya tienes puedes acceder al envio gratis? si solo estas interesado en uno de nuestros libros no pasa nada, tu total de compra con envío agregado es de $${total + 1000}\na continuación ingrese los datos pedidos para poder hacer el envío posible.`);
+        const nombre = prompt("Nombre completo:")
+        const direcciónCompleta = prompt("Dirección completa, incluyendo calle, número de casa o apartamento, ciudad, estado o provincia:");
+        const codigoPostal = prompt("Codigo Postal:");
+        const teléfono = prompt("Número telefónico:");
+
+        alert(`Los datos ingresados fueron:\nNombre: ${nombre}\nDirección: ${direcciónCompleta}\nCodigo postal: ${codigoPostal}\nNúmero de teléfono: ${teléfono}\nEn unos instantes te llamaremos para concretar el envío, gracias por tu compra.`);
+
+    }
+
+}
+
+
+realizarEnvio();
