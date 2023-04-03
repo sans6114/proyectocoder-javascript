eb3610915989d52b2f4694bb083369187299caad
Santiago Sosa <santisosalp@gmail.com>
2023-04-03 16:04:58 -0300

    se hicieron todos los cambios para la segunda entrega

--- main.js
+++ main.js
@@ -1,81 +1,52 @@
 //algoritmo para mi tienda de libros de la editorial edicionesvs//
-
-
-
-//Mensaje de bienvenida para el usuario//
-
-alert("Hola! Bienvenido al carrito de compras de ediciones VS.\nIngresa a continuación el número del producto en el que estés interesado, si deseas salir ingresa 0 en tu teclado.")
-
-//selección de productos de parte del usuario junto al mensaje//
-let seleccionarProductos = Number(prompt("1-¿Qué tienes en tu mano? (Mariana Maillet) valor: $1500\n2-Antídotos contra la religión (Juan Pablo Sosa) valor: $1500\n3-Mujer Maravilla (Natalia Spetale) valor: $1500\n4-Hijos de la Promesa (Ezequiel Rossini) valor: $1500\n5-La vida de Grace (Natalia Spetale) valor: $1500\n6-Diseñados para una vida Saludable (María José Amiunes) valor: $1500"))
-
-
-let seleccionarCantidad;
-let total = 0;
 const productos = [
-    "¿Qué tienes en tu mano? (Mariana Maillet)",
-    "Antídotos contra la religión (Juan Pablo Sosa)",
-    "Mujer Maravilla (Natalia Spetale)",
-    "Hijos de la Promesa (Ezequiel Rossini)",
-    "La vida de Grace (Natalia Spetale)",
-    "Diseñados para una vida Saludable (María José Amiunes)"
+    { id : 1,   titulo : "¿Qué tienes en tu mano? (Mariana Maillet)", precio : 1500 },
+    { id : 2,  titulo: "Antídotos contra la religión (Juan Pablo Sosa)", precio : 1500 },
+    { id : 3,  titulo: "Mujer Maravilla (Natalia Spetale)", precio : 1500 },
+    { id : 4,  titulo: "Hijos de la Promesa (Ezequiel Rossini)", precio : 1500 },
+    { id : 5, titulo: "La vida de Grace (Natalia Spetale)", precio : 1500 },
+    { id : 6, titulo: "Diseñados para una vida Saludable (María José Amiunes)", precio : 1500 }
 ];
 
+let carritoDeCompras = [];
 
-const cantidad = (cant, precio) => {
-    return cant * precio
-}
+let seleccionado;
 
-
-
-while (seleccionarProductos != 0) {
-    switch (seleccionarProductos) {
-        case 1:
-            seleccionarCantidad = Number(prompt("el producto seleccionado es ¿Qué tienes en tu mano? (Mariana Maillet), indique la cantidad"))
-            total = total + cantidad(seleccionarCantidad, 1500)
-            tituloLibro = "¿Qué tienes en tu mano? (Mariana Maillet)"
-            alert("¡Has agregado " + seleccionarCantidad + " copia(s) de " + tituloLibro + " al carrito!")
-            break;
-        case 2:
-            seleccionarCantidad = Number(prompt("el producto seleccionado es Antidotos contra la religión (Juan Pablo Sosa), indique la cantidad"))
-            total = total += cantidad(seleccionarCantidad, 1500)
-            tituloLibro = "Antidotos contra la religión (Juan Pablo Sosa)"
-            alert("¡Has agregado " + seleccionarCantidad + " copia(s) de " + tituloLibro + " al carrito!")
-            break;
-        case 3:
-            seleccionarCantidad = Number(prompt("el producto seleccionado es Mujer Maravilla (Natalia Spetale), indique la cantidad"))
-            total = total += cantidad(seleccionarCantidad, 1500)
-            tituloLibro = "Mujer Maravilla (Natalia Spetale)"
-            alert("¡Has agregado " + seleccionarCantidad + " copia(s) de " + tituloLibro + " al carrito!")
-            break;
-        case 4:
-            seleccionarCantidad = Number(prompt("el producto seleccionado es Hijos de la Promesa (Ezequiel Rossini), indique la cantidad"))
-            total = total += cantidad(seleccionarCantidad, 1500)
-            tituloLibro = "Hijos de la Promesa (Ezequiel Rossini)"
-            alert("¡Has agregado " + seleccionarCantidad + " copia(s) de " + tituloLibro + " al carrito!")
-            break;
-        case 5:
-            seleccionarCantidad = Number(prompt("el producto seleccionado es La vida de Grace (Natalia Spetale), indique la cantidad"))
-            total = total += cantidad(seleccionarCantidad, 1500)
-            tituloLibro = "La vida de Grace (Natalia Spetale)"
-            alert("¡Has agregado " + seleccionarCantidad + " copia(s) de " + tituloLibro + " al carrito!")
-            break;
-        case 6:
-            seleccionarCantidad = Number(prompt("el producto seleccionado es Diseñados para una vida Saludable (María José Amiunes), indique la cantidad"))
-            total = total += cantidad(seleccionarCantidad, 1500)
-            tituloLibro = "Diseñados para una vida Saludable (María José Amiunes)"
-            alert("¡Has agregado " + seleccionarCantidad + " copia(s) de " + tituloLibro + " al carrito!")
-            break;
-
-        default:
-            alert("ups, ingresaste un número incorrecto, te invito a ver los titulos nuevamente para que puedas ingresarlo nuevamente " + productos)
-            break;
-
+do {
+    seleccionado = prompt("Hola! Bienvenido al carrito de compras de ediciones VS.\nEstás interesado en algún producto? (ingresar si o no)");
+    if (seleccionado !== "si" && seleccionado !== "no") {
+        alert("Ups, ingresaste un dato equivocado, por favor ingresa 'si' o 'no'");
     }
-    seleccionarProductos = Number(prompt("1-¿Qué tienes en tu mano? (Mariana Maillet) valor: $1500\n2-Antidotos contra la religión (Juan Pablo Sosa) valor: 1500$\n3-Mujer Maravilla (Natalia Spetale) valor: $1500\n4-Hijos de la Promesa (Ezequiel Rossini) valor: $1500\n5-La vida de Grace (Natalia Spetale) valor: $1500\n6-Diseñados para una vida Saludable (María José Amiunes) valor: $1500"))
-}
+} while (seleccionado !== "si" && seleccionado !== "no");
 
-alert(`¡Gracias por tu compra! Has adquirido productos en Ediciones VS.\nPrecio de cada libro: $1500\nEl total de tu compra es: $${total}. ¡Esperamos que disfrutes de tu compra!`);
+if (seleccionado === "si") {
+    alert("Aquí está la lista de nuestros productos:");
+
+    for (let i = 0; i < productos.length; i++) {
+        alert(productos[i].id + ": " + productos[i].titulo + " - $" + productos[i].precio);
+    }
+
+    let agregarProducto;
+
+    do {
+        let idProducto = prompt("Por favor ingresa el ID del producto que deseas agregar al carrito:");
+        let productoSeleccionado = productos.find(producto => producto.id === parseInt(idProducto));
+
+        if (productoSeleccionado) {
+            carritoDeCompras.push(productoSeleccionado);
+            alert("¡Felicitaciones! Agregaste correctamente el producto al carrito");
+        } else {
+            alert("Ups, ingresaste un ID incorrecto");
+        }
+
+        agregarProducto = prompt("¿Deseas agregar otro producto al carrito? (si/no)");
+    } while (agregarProducto === "si");
+
+    let totalCompra = carritoDeCompras.reduce((total, producto) => total + producto.precio, 0);
+    alert("El total de la compra es $" + totalCompra);
+} else {
+    alert("¡Esperamos verte pronto, hasta luego!");
+}
 
 //algoritmo para envío//
 
@@ -106,4 +77,9 @@ const realizarEnvio = () => {
 }
 
 
+
 realizarEnvio();
+
+//practica de objetos con método de pago//
+
+
